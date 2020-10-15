b6de3fcd69fac3091f8534066a8e5e85932668b8
muhammadkorra <muhammadkorra@gmail.com>
2020-10-15 10:47:43 +0200

    Would rely on Docker SDK models instead of creating my own models

--- classes/service.py
+++
@@ -1,9 +0,0 @@
-class Service:
-    """
-    A Service class to represent a Docker Service
-    """
-    def __init__(self):
-        pass
-
-    def __repr__(self):
-        pass
--- classes/volume.py
+++
@@ -1,9 +0,0 @@
-class Volume:
-    """
-    A Volume class to represent a Docker volume inside the Docker Compose file
-    """
-    def __init__(self, name="", volume={}):
-        pass
-
-    def __repr__(self):
-        pass
