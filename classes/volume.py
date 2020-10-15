de0701a911c7a828db8930627fb81db8082e4563
muhammadkorra <muhammadkorra@gmail.com>
2020-10-15 02:08:21 +0200

    changed the name to volume.py

---
+++ classes/volume.py
@@ -0,0 +1,9 @@
+class Volume:
+    """
+    A Volume class to represent a Docker volume inside the Docker Compose file
+    """
+    def __init__(self, name="", volume={}):
+        pass
+
+    def __repr__(self):
+        pass
